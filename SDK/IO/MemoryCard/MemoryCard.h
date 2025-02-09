a9d108d6acb1f4c48e35393f6c7a9c0cec19214e
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2025-02-09 01:37:42 -0500

    added memory card header

---
+++ SDK/IO/MemoryCard/MemoryCard.h
@@ -0,0 +1,10 @@
+#ifndef KAR_SDK_IO_MEMORYCARD
+#define KAR_SDK_IO_MEMORYCARD
+
+//defines various structs and functions for memory cards
+
+//defines ports for where memory cards would go
+#define GC_SYSTEM_EXTRA_SLOT_PORT_A, 0
+#define GC_SYSTEM_EXTRA_SLOT_PORT_B, 1
+
+#endif
